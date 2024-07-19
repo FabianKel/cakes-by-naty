04d001a383018316a42e5e9293fdb1e82f07a67b
edman-cota <edmancota2017@gmail.com>
2024-07-18 22:23:07 -0600

    actualizamos la forma de acceder a la base de datos

--- server/index.js
+++ server/index.js
@@ -1,9 +1,54 @@
 const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
+const { Pool } = require('pg');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
 const app = express();
 const port = 4000;
 
-app.get('/', (req, res) => {
-  res.send('Hello, World!');
+app.use(cors());
+app.use(bodyParser.json());
+
+const pool = new Pool({
+  // user: process.env.DB_USER,
+  // host: process.env.DB_HOST,
+  // database: process.env.DB_NAME,
+  // password: process.env.DB_PASSWORD,
+  // port: process.env.DB_PORT,
+  user: 'admin',
+  host: '127.0.0.1',
+  database: 'cakes-by-naty',
+  password: 'admin1234',
+  port: 5433,
+});
+
+app.use(bodyParser.json());
+
+app.post('/register', async (req, res) => {
+  const { email, password, first_name, last_name } = req.body;
+  const hashedPassword = await bcrypt.hash(password, 10);
+
+  console.log('email: ', email);
+  console.log('hashedPassword: ', hashedPassword);
+
+  try {
+    const client = await pool.connect();
+
+    const newUser = await client.query(
+      'INSERT INTO users (first_name, last_name, email, password) VALUES ($1 ,$2, $3, $4) RETURNING *',
+      [first_name, last_name, email, hashedPassword]
+    );
+
+    client.release();
+    res.status(201).json(result.rows[0]);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
 });
 
 app.listen(port, () => {
