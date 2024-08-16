0315da80ff7a9a34908752059fd8fb6d73c79776
Isabella-22293 <116053798+Isabella-22293@users.noreply.github.com>
2024-08-16 01:44:49 -0600

    page/gracias

---
+++ frontend/src/app/Gracias/page.js
@@ -0,0 +1,13 @@
+import Header from '@/components/Header'
+import Footer from '@/components/Footer'
+import Gracias from '@/components/Gracias'
+
+export default function Home() {
+  return (
+    <>
+    <Header/>
+    <Gracias/>
+    <Footer/>
+    </>
+  )
+}
