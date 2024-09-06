95136cfd4dbc5cfa5b0d41dbb42ce304aeace968
Isabella-22293 <116053798+Isabella-22293@users.noreply.github.com>
2024-09-06 14:22:23 -0600

    Page de confirmacion

---
+++ frontend/src/app/Confirmacion/page.js
@@ -0,0 +1,13 @@
+import Header from '@/components/common/Header'
+import Footer from '@/components/common/Footer'
+import Confirmacion from '@/components/sections/Confirmacion'
+
+export default function Home() {
+  return (
+    <>
+    <Header/>
+    <Confirmacion/>
+    <Footer/>
+    </>
+  )
+}
