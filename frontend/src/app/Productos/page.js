dbf3d4ef2ed006ecca066b71318884d0094fbe68
Isabella-22293 <116053798+Isabella-22293@users.noreply.github.com>
2024-09-06 13:57:52 -0600

    Page de producto

---
+++ frontend/src/app/Productos/page.js
@@ -0,0 +1,13 @@
+import Header from '@/components/common/Header'
+import Footer from '@/components/common/Footer'
+import Cakepops from '@/components/sections/Cakepops'
+
+export default function Home() {
+  return (
+    <>
+    <Header/>
+    <Cakepops/>
+    <Footer/>
+    </>
+  )
+}
