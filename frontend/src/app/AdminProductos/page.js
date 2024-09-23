9b9a73b81ef8222a9ba74952d9bc1f112dbd065a
FabianKel <drk.fab@gmail.com>
2024-09-23 17:45:50 -0600

    AdminProducts CRUD moved
    Add Products Page.js to merge

---
+++ frontend/src/app/AdminProductos/page.js
@@ -0,0 +1,13 @@
+import Header from '@/components/common/Header'
+import ProductCRUD from '@/components/sections/ProductCRUD'
+import Footer from '@/components/common/Footer'
+
+export default function Productos() {
+    return (
+        <>
+            <Header />
+            <ProductCRUD />
+            <Footer />
+        </>
+    )
+}
