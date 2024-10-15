af8372e86e0e59e00dc567907f0aaad3b06cbe25
edman-cota <edmancota2017@gmail.com>
2024-10-15 09:44:27 -0600

    ultimos cambios

--- frontend/src/components/common/CatalogCategories.jsx
+++ frontend/src/components/common/CatalogCategories.jsx
@@ -1,29 +1,38 @@
-import Link from 'next/link';
+'use client';
 
-function CatalogCategories() {
-    const categories = [
-        { name: "Cupcakes", image: "Catalog/catalogcupcake.png", url:"/catalog/cupcakes" },
-        { name: "Cakepops", image: "Catalog/catalogcakepop.png", url:"/catalog/cakepops" },
-        { name: "Pastel", image: "Catalog/catalogcake.png", url:"/catalog/pasteles" },
-        { name: "Galletas", image: "Catalog/catalogcookies.png", url:"/catalog/galletas" },
-        { name: "Chocolates", image: "Catalog/catalogchocolates.png" , url:"/catalog/chocolates"},
-        { name: "Espumillas", image: "Catalog/catalogespumillas.png", url:"/catalog/espumillas" }
-    ];
+import { useRouter } from 'next/navigation'; // Nueva API para la estructura de rutas `app/`
 
-    return (
-        <div className="flex justify-center space-x-6 mt-8 mb-8">
-            {categories.map((category, index) => (
-                <div key={index} className="flex flex-col items-center">
-                    <div className="w-28 h-28 bg-white-200 flex justify-center items-center rounded-lg transition-transform transform hover:scale-110">
-                        <Link href={category.url}>
-                            <img src={category.image} alt={category.name} className="w-30 h-30" />
-                        </Link>
-                    </div>
-                    <span className="mt-2 text-sm font-medium">{category.name}</span>
-                </div>
-            ))}
+const CatalogCategories = () => {
+  const router = useRouter(); // Nueva versión del hook
+
+  const handleFilterChange = (filter) => {
+    console.log('filter: ', filter);
+    router.push(`/catalog?filter=${filter}`);
+  };
+
+  const categories = [
+    { name: 'Cupcakes', image: 'Catalog/catalogcupcake.png', url: 'cupcakes' },
+    { name: 'Cakepops', image: 'Catalog/catalogcakepop.png', url: 'cakepops' },
+    { name: 'Pastel', image: 'Catalog/catalogcake.png', url: 'pasteles' },
+    { name: 'Galletas', image: 'Catalog/catalogcookies.png', url: 'galletas' },
+    { name: 'Chocolates', image: 'Catalog/catalogchocolates.png', url: 'chocolates' },
+    { name: 'Espumillas', image: 'Catalog/catalogespumillas.png', url: 'espumillas' },
+  ];
+
+  return (
+    <div className='flex justify-center space-x-6 mt-8 mb-8'>
+      {categories.map((category, index) => (
+        <div key={index} className='flex flex-col items-center'>
+          <div className='w-28 h-28 bg-white-200 flex justify-center items-center rounded-lg transition-transform transform hover:scale-110'>
+            <button onClick={() => handleFilterChange(category.url)}>
+              <img src={category.image} alt={category.name} className='w-30 h-30' />
+            </button>
+          </div>
+          <span className='mt-2 text-sm font-medium'>{category.name}</span>
         </div>
-    );
-}
+      ))}
+    </div>
+  );
+};
 
 export default CatalogCategories;
--- frontend/src/components/sections/Catalog.jsx
+++ frontend/src/components/sections/Catalog.jsx
@@ -2,40 +2,74 @@
 
 import React, { useEffect, useState } from 'react';
 import ProductGroup from '@/components/common/ProductGroup';
+import { useSearchParams } from 'next/navigation';
 
 function Catalogo() {
-    const [cakes, setCakes] = useState([]);
-    const [cupcakes, setCupcakes] = useState([]);
+  const searchParams = useSearchParams();
 
-    useEffect(() => {
-        fetch('http://localhost:4000/products/categoria/3/3')
-            .then((response) => response.json())
-            .then((data) => {
-                console.log('Cakes:', data); 
-                setCakes(data.productos || []); 
-            })
-            .catch((error) => console.error('Error fetching cakes:', error));
+  const [cakes, setCakes] = useState([]);
+  const [cupcakes, setCupcakes] = useState([]);
+  const [products, setProducts] = useState([]);
 
-        fetch('http://localhost:4000/products/categoria/1/3')
-            .then((response) => response.json())
-            .then((data) => {
-                console.log('Cupcakes:', data); 
-                setCupcakes(data.productos || []); 
-            })
-            .catch((error) => console.error('Error fetching cupcakes:', error));
-    }, []);
+  const categories = {
+    cupcakes: { nombre: 'Nuestos Cupcakes', id: 1 },
+    cakepops: { nombre: 'Nuestros Cakepops', id: 2 },
+    pasteles: { nombre: 'Nuestros Pastel', id: 3 },
+    galletas: { nombre: 'Nuestras Galletas', id: 4 },
+    chocolates: { nombre: 'Nuestros Chocolates', id: 5 },
+    espumillas: { nombre: 'Nuestras Espumillas', id: 6 },
+  };
 
-    return (
-        <div className='flex flex-col'>
-            <div className="container mx-auto p-4">
-                <h1 className="text-4xl text-center font-bold mt-6 mb-6">Catálogo</h1>
-                <h2 className="text-xl font-bold mb-4">Nuestros Pasteles</h2>
-                <ProductGroup products={cakes} />
-                <h2 className="text-xl font-bold mb-4">Nuestros Cupcakes</h2>
-                <ProductGroup products={cupcakes} />
-            </div>
+  const filter = searchParams.get('filter');
+
+  const fetchProducts = (filter) => {
+    if (filter) {
+      fetch(`http://localhost:4000/products/categoria/${categories[filter].id}`)
+        .then((response) => response.json())
+        .then((data) => {
+          setCakes(data.productos || []);
+        })
+        .catch((error) => console.error('Error fetching cakes:', error));
+    } else {
+      fetch('http://localhost:4000/products/categoria/3/3')
+        .then((response) => response.json())
+        .then((data) => {
+          setCakes(data.productos || []);
+        })
+        .catch((error) => console.error('Error fetching cakes:', error));
+
+      fetch('http://localhost:4000/products/categoria/1/3')
+        .then((response) => response.json())
+        .then((data) => {
+          setCupcakes(data.productos || []);
+        })
+        .catch((error) => console.error('Error fetching cupcakes:', error));
+    }
+  };
+
+  useEffect(() => {
+    fetchProducts(filter);
+  }, [filter]);
+
+  return (
+    <div className='flex flex-col'>
+      {filter ? (
+        <div className='container mx-auto p-4'>
+          <h1 className='text-4xl text-center font-bold mt-6 mb-6'>Catálogo</h1>
+          <h2 className='text-xl font-bold mb-4'>{categories[filter].nombre}</h2>
+          <ProductGroup products={cakes} />
         </div>
-    );
+      ) : (
+        <div className='container mx-auto p-4'>
+          <h1 className='text-4xl text-center font-bold mt-6 mb-6'>Catálogo</h1>
+          <h2 className='text-xl font-bold mb-4'>Nuestros Pasteles</h2>
+          <ProductGroup products={cakes} />
+          <h2 className='text-xl font-bold mb-4'>Nuestros Cupcakes</h2>
+          <ProductGroup products={cupcakes} />
+        </div>
+      )}
+    </div>
+  );
 }
 
 export default Catalogo;
