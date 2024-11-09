ba6a649b9f5a817ff79bb77253256376eaf9f7c0
edman-cota <edmancota2017@gmail.com>
2024-11-08 21:39:09 -0600

    agregamos endpoint de obtener un pedido por id

--- server/controllers/ordersController.js
+++ server/controllers/ordersController.js
@@ -27,18 +27,36 @@ const getAllOrders = async (req, res) => {
   }
 };
 
+// Obtener los productos de un pedido en especifico
+const getOrderProducts = async (req, res) => {
+  try {
+    const { pedido_id } = req.params;
+    console.log('pedioid: ', pedido_id);
+
+    const client = await pool.connect();
+    let query = `SELECT * FROM obtener_productos_por_pedido(${pedido_id})`;
+    const result = await client.query(query);
+    console.log('result: ', result);
+    const Productos = result.rows;
+    client.release();
+    res.status(200).json({ message: 'Productos obtenidos con éxito', Productos });
+  } catch (error) {
+    ErrorHandler.handleError(error, res);
+  }
+};
+
 const getByStatus = async (req, res) => {
-    const { estado } = req.params;
-    try {
-      const client = await pool.connect();
-      const getPedidosQuery = 'SELECT * FROM obtener_pedidos_por_estado($1);';
-      const result = await client.query(getPedidosQuery, [estado]);
-      const Pedidos = result.rows;
-      client.release();
-      res.status(200).json({ message: 'Pedidos obtenidos con éxito', Pedidos: Pedidos });
-    } catch (error) {
-      ErrorHandler.handleError(error, res);
-    }
+  const { estado } = req.params;
+  try {
+    const client = await pool.connect();
+    const getPedidosQuery = 'SELECT * FROM obtener_pedidos_por_estado($1);';
+    const result = await client.query(getPedidosQuery, [estado]);
+    const Pedidos = result.rows;
+    client.release();
+    res.status(200).json({ message: 'Pedidos obtenidos con éxito', Pedidos: Pedidos });
+  } catch (error) {
+    ErrorHandler.handleError(error, res);
+  }
 };
 
 //Borrar por ID
@@ -63,9 +81,9 @@ const deleteOrderById = async (req, res) => {
   }
 };
 
-
 module.exports = {
-    getAllOrders,
-    getByStatus,
-    deleteOrderById
-}
+  getAllOrders,
+  getOrderProducts,
+  getByStatus,
+  deleteOrderById,
+};
--- server/routes/orders.js
+++ server/routes/orders.js
@@ -1,8 +1,14 @@
 const express = require('express');
 const router = express.Router();
-const { getAllOrders, getByStatus, deleteOrderById } = require('../controllers/ordersController');
+const {
+  getAllOrders,
+  getByStatus,
+  deleteOrderById,
+  getOrderProducts,
+} = require('../controllers/ordersController');
 
 router.get('/', getAllOrders);
+router.get('/:pedido_id', getOrderProducts);
 router.get('/estado/:estado', getByStatus);
 router.delete('/:id', deleteOrderById);
 
